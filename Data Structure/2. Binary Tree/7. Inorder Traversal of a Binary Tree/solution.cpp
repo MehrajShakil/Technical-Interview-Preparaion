2d5a4803598859ceeb718aabd34b9946fe070a55
Md. Mehraj Hossain <mdmehrajshakil98@gmail.com>
2022-05-02 16:29:17 +0600

    tree trversal in,pre,post order added

---
+++ Data Structure/2. Binary Tree/7. Inorder Traversal of a Binary Tree/solution.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void printTree(Node* node) {
+    if(!node) return;
+    cout << node->data << "\n";
+    printTree(node->left);
+    printTree(node->right);
+}
+
+vector<int> inOrder;
+void dfs(Node* node) {
+    if(!node) return;
+    dfs(node->left);
+    inOrder.push_back(node->data);
+    dfs(node->right);
+}
+
+
+int main() {
+
+    /*
+          1
+        /   \
+      2       3
+     /  \
+    4    5
+   /    /
+  9    6
+ /    /
+7    8
+    Inorder (Left, Root, Right)
+  */
+
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->left->left = new Node(4);
+    root->left->left->left = new Node(9);
+    root->left->left->left->left = new Node(7);
+    root->right = new Node(3);
+    root->left->right = new Node(5);
+    root->left->right->left = new Node(6);
+    root->left->right->left->left = new Node(8);
+
+    puts("Print the tree");
+    printTree(root);
+    dfs(root);
+    for ( auto it : inOrder ) cout << it << " ";
+}
